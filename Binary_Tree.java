30f8de0172cc580a1d7b59094ea844a60ea0a18e
diegolopez96 <lop141222@uvg.edu.gt>
2016-09-26 22:04:03 -0500

    Update Binary_Tree.java

--- Binary_Tree.java
+++ Binary_Tree.java
@@ -1,16 +1,18 @@
 /* 	Universidad del Valle de Guatemala
- *  Algoritmos y Estructura de Datos
- *  	Pedro Antonio Garcia Morales 15409
- *  	Diego Lopez 
+ *  	Algoritmos y Estructura de Datos
+ *  	Pedro Antonio Garcia Morales 	15409
+ *  	Diego Lopez 			141222
  *  	Hoja de Trabajo 7. Binary Tree
 */
 
 public class Binary_Tree<E> {
     
-	 private Nodo node;
+     //Variables
+     private Nodo node;
      private String string;
      private String btree;
  
+    //Constructor
     public  void addNodo(Association<String,String> valueAdd){
         if(node == null){
             node = new Nodo(valueAdd);
@@ -20,12 +22,14 @@ public class Binary_Tree<E> {
         }  
     }
 
+    //Método que busca las palabras para traducir
     public String Buscar(String cadena){
         string = cadena;
         B_Help(node);
         return string;   
        }
     
+   //Método que ayuda a buscar las palabras de ingles a español en el diccionario 
    public void B_Help(Nodo nodo){
        if(nodo == null) {
     	   return  ;
