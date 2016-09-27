2f5096b12e913fccdfdf423d9b97f8923967153c
diegolopez96 <lop141222@uvg.edu.gt>
2016-09-26 22:06:46 -0500

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,22 +1,24 @@
 /* 	Universidad del Valle de Guatemala
- *  Algoritmos y Estructura de Datos
- *  	Pedro Antonio Garcia Morales 15409
- *  	Diego Lopez 
+ *  	Algoritmos y Estructura de Datos
+ *  	Pedro Antonio Garcia Morales 	15409
+ *  	Diego Lopez 			141222
  *  	Hoja de Trabajo 7. Binary Tree
 */
 
 public class Nodo {
-	
+    //Variables
     Nodo izq;
     Nodo der;
     Association<String,String> datos=new Association<>();
   
+    
     public Nodo(Association valorNodo){
         datos = valorNodo;
         izq = null;
         der = null; 
     }     
    
+    //Compara las palabras para poderlas cambiar al traducir 
     public  void InsNueNode(Association<String,String> InsValor) {
         
         String word = InsValor.getIngles();
